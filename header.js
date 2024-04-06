467620e384a27d475c904b683124306727fc0458
2100031047 <109621968+2100031047@users.noreply.github.com>
2024-04-06 12:07:30 +0530

    Add files via upload

---
+++ header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+const Header = () => {
+  return (
+    <div className="header" style={{ backgroundImage: 'url("path/to/your/image.jpg")' }}>
+      {/* Your header content here */}
+      <h1>█▓▒▒░░░GRADEBOOK PROJECT░░░▒▒▓█</h1>
+    </div>
+  );
+};
+
+export default Header;
